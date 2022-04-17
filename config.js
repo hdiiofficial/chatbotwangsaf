abbe6355e9e7fa401b5131cb75873ac7ce40e7d0
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-04-17 17:20:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ global.APIKeys = {
 }
 
 // Other
+global.nohadi = ['6285701399751'] //yang ini jan di ganti cok
 global.owner = ['6285701399751'] //mau jdi own bilng aj jan gnti disini
 global.premium = ['6285701399751']
 global.packname = 'Anonymous Chat'
