c36133a00735d7472d6d1c6c620279f5ed96ce21
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-05-12 00:59:51 +0700

    Update hisoka.js

--- hisoka.js
+++ hisoka.js
@@ -164,6 +164,62 @@ hisoka.sendMessage('6285701399751@s.whatsapp.net', options, text, {quoted: m})
 m.reply('#REPORT_USER\n\n\nLAPORAN ANDA TELAH SAMPAI KE OWNER | LAPORAN PALSU ATAU MAIN² AKAN KE KENAI SANKSI.')
 }
 break
+            case 'ping': case 'botstatus': case 'statusbot': case 'infobot': {
+                const used = process.memoryUsage()
+                const cpus = os.cpus().map(cpu => {
+                    cpu.total = Object.keys(cpu.times).reduce((last, type) => last + cpu.times[type], 0)
+			        return cpu
+                })
+                const cpu = cpus.reduce((last, cpu, _, { length }) => {
+                    last.total += cpu.total
+                    last.speed += cpu.speed / length
+                    last.times.user += cpu.times.user
+                    last.times.nice += cpu.times.nice
+                    last.times.sys += cpu.times.sys
+                    last.times.idle += cpu.times.idle
+                    last.times.irq += cpu.times.irq
+                    return last
+                }, {
+                    speed: 0,
+                    total: 0,
+                    times: {
+			            user: 0,
+			            nice: 0,
+			            sys: 0,
+			            idle: 0,
+			            irq: 0
+                }
+                })
+                let timestamp = speed()
+                let latensi = speed() - timestamp
+                neww = performance.now()
+                oldd = performance.now()
+                respon = `
+\`\`\`Anonymous Chat 1.1.1 INFORMATION\`\`\`\
+
+Name : Anonymous Chat
+Version : 1.1.1
+Owner : hdiiofficial
+Base : Baileys
+Session : WhatsApp Web-Multi Device
+Kecepatan bot ${latensi.toFixed(4)} _Second_ \n ${oldd - neww} _miliseconds_
+Bot ini aktif sejak ${runtime(process.uptime())} ynang lalu.
+
+💻 Info Server
+THIS BOT USING VPS SERVER
+RAM: ${formatp(os.totalmem() - os.freemem())} / ${formatp(os.totalmem())}
+
+_NodeJS Memory Usaage_
+${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${formatp(used[key])}`).join('\n')}
+
+${cpus[0] ? `_Total CPU Usage_
+${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+_CPU Core(s) Usage (${cpus.length} Core CPU)_
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
+                `.trim()
+                m.reply(respon)
+            }
+            break
                 case 'setppbot': {
                 if (!isCreator) throw mess.owner
                 if (!quoted) throw `Kirim/Reply Image Dengan Caption ${prefix + command}`
